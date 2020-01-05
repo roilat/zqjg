d4aac78049d81d4c846e2608e89f2e11116c79c1
liujing <tjkj10@126.com>
2020-01-06 00:10:54 +0800

    修改属性值

--- cqzqjg-services/src/main/java/cn/roilat/cqzqjg/services/biz/vo/HomePageVo.java
+++ cqzqjg-services/src/main/java/cn/roilat/cqzqjg/services/biz/vo/HomePageVo.java
@@ -19,47 +19,60 @@ public class HomePageVo {
     private List<HomePageVo> homePageVo;
     private List<Main> main;
     private List<News> news;
-	public List<AboutUs> getAboutUs() {
-		return aboutUs;
-	}
-	public void setAboutUs(List<AboutUs> aboutUs) {
-		this.aboutUs = aboutUs;
-	}
-	public List<ChooseUs> getChooseUs() {
-		return chooseUs;
-	}
-	public void setChooseUs(List<ChooseUs> chooseUs) {
-		this.chooseUs = chooseUs;
-	}
-	public List<Culture> getCulture() {
-		return culture;
-	}
-	public void setCulture(List<Culture> culture) {
-		this.culture = culture;
-	}
-	public List<DealScene> getDealScene() {
-		return dealScene;
-	}
-	public void setDealScene(List<DealScene> dealScene) {
-		this.dealScene = dealScene;
-	}
-	public List<HomePageVo> getHomePageVo() {
-		return homePageVo;
-	}
-	public void setHomePageVo(List<HomePageVo> homePageVo) {
-		this.homePageVo = homePageVo;
-	}
-	public List<Main> getMain() {
-		return main;
-	}
-	public void setMain(List<Main> main) {
-		this.main = main;
-	}
-	public List<News> getNews() {
-		return news;
-	}
-	public void setNews(List<News> news) {
-		this.news = news;
-	}
-    
+
+    public List<DealScene> getDealScene() {
+        return dealScene;
+    }
+
+    public void setDealScene(List<DealScene> dealScene) {
+        this.dealScene = dealScene;
+    }
+
+    public List<HomePageVo> getHomePageVo() {
+        return homePageVo;
+    }
+
+    public void setHomePageVo(List<HomePageVo> homePageVo) {
+        this.homePageVo = homePageVo;
+    }
+
+    public List<Main> getMain() {
+        return main;
+    }
+
+    public void setMain(List<Main> main) {
+        this.main = main;
+    }
+
+    public List<News> getNews() {
+        return news;
+    }
+
+    public void setNews(List<News> news) {
+        this.news = news;
+    }
+
+    public AboutUs getAboutUs() {
+        return aboutUs;
+    }
+
+    public void setAboutUs(AboutUs aboutUs) {
+        this.aboutUs = aboutUs;
+    }
+
+    public ChooseUs getChooseUs() {
+        return chooseUs;
+    }
+
+    public void setChooseUs(ChooseUs chooseUs) {
+        this.chooseUs = chooseUs;
+    }
+
+    public Culture getCulture() {
+        return culture;
+    }
+
+    public void setCulture(Culture culture) {
+        this.culture = culture;
+    }
 }
