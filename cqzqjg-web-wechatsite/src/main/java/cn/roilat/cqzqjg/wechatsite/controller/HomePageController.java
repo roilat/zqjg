067c028fd2102d965a938a813bee6eb149224612
roilat-D <123456>
2020-01-07 21:37:16 +0800

    ''

--- cqzqjg-web-wechatsite/src/main/java/cn/roilat/cqzqjg/wechatsite/controller/HomePageController.java
+++ cqzqjg-web-wechatsite/src/main/java/cn/roilat/cqzqjg/wechatsite/controller/HomePageController.java
@@ -1,19 +1,18 @@
 package cn.roilat.cqzqjg.wechatsite.controller;
 
-import cn.roilat.cqzqjg.core.http.HttpResult;
-import cn.roilat.cqzqjg.services.biz.service.BizPortalInfoService;
-import cn.roilat.cqzqjg.services.biz.vo.HomePageVo;
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
-import com.alibaba.fastjson.serializer.SerializerFeature;
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import cn.roilat.cqzqjg.core.http.HttpResult;
+import cn.roilat.cqzqjg.services.biz.service.BizPortalInfoService;
+import cn.roilat.cqzqjg.services.biz.vo.HomePageVo;
 
 /**
  * @program: zqjg
